742f5d68f69e09086a364cee40c30ab53b173f6f
Archit Agarwal <archit.agarwal.met14@iitbhu.ac.in>
2018-06-21 10:44:54 +0530

    init

---
+++ ContestAPI/src/main/java/com/constest/ContestAPI/ContestApiApplication.java
@@ -0,0 +1,12 @@
+package com.constest.ContestAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContestApiApplication.class, args);
+	}
+}
---
+++ ContestAPI/src/test/java/com/constest/ContestAPI/ContestApiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.constest.ContestAPI;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ContestApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
